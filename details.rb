81201c6d4be1cf378fb717e4e85c43467a5b1b83
KrisHolley <krisholley_09@hotmail.com>
2018-09-25 11:39:38 -0400

    details

--- details.rb
+++ details.rb
@@ -1,7 +1,11 @@
-def my_details
-	puts"Kris Holley"
-	puts"Address"
-	puts"Phone Number"
+def my_details(fname, address, phone)
+	puts"Name: #{fname}"
+	puts"Address: #{address}"
+	puts"Phone Number: #{phone}"
 end
 
-my_details
+my_details("Kris", "77 Circle", "304-")
+
+my_details("Sweeny Todd", "Fleet st", "877-cuts")
+
+my_details("John", "81 Maple Ln", "681-")
